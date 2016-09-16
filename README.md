7c606b4c454fc84340749340f6cf2936175eb12b
Ricky Chilcott <ricky@rbsavvy.com>
2016-09-16 00:40:23 -0400

    Adding a homepage and signup form.
    Using Hapi, Mongoose, and some validators to ensure data is being saved into MongoDB correctly.
    Using PureCSS for styling

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # coffeetalk
-Matching people up for coffee
+Matching people up for coffee.
+
+People signup with their name/email and then they get an email, every Monday to opt in/out for the Friday meetup.
 
 
 People collection:
 
 ```json
 people: [
-  { 
+  {
     "name": "name",
     "email": "email",
     "availability": "morning" / "afternoon" / "both" / "none",
     "previousMatches": [
-        <person> 
+        <person>
     ]
   }, ...
 ]
---
+++ app.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const hapi      = require('hapi');
+const server    = new hapi.Server();
+const validator = require('validator');
+const mongoose  = require('mongoose');
+const Person    = mongoose.model('Person',
+                      { name: {
+                          type: String,
+                          required: true
+                        },
+                        email: {
+                          type: String,
+                          required: true,
+                          validator: (value) =>
+                             validator.isEmail(value)
+                        },
+                        availability: {
+                          type: String,
+                          enum: ["morning", "afternoon", "both", "none"]
+                        }
+                     }
+                  );
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/coffee-talk');
+
+server.connection({ port: process.env.PORT || 3000 });
+
+server.register([require('vision'), require('inert')], (err) => {
+  server.views({
+    engines: {
+        ejs: require('ejs')
+    },
+    relativeTo: __dirname,
+    path: 'templates'
+  });
+
+  server.route({
+      method: 'GET',
+      path: '/',
+      handler: {
+          view: 'index'
+      }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/signup',
+    handler: function (request, reply) {
+      const { name, email, availability } = request.payload
+      const person = new Person({ name, email, availability })
+
+      person.save(function (err) {
+        if (err) {
+          console.log(err)
+          reply.redirect('/?signup=failure')
+        } else {
+          reply.redirect('/?signup=success')
+        }
+      })
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'assets',
+        listing: true
+      }
+    }
+  });
+});
+
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+
+  console.log(`Server running at: ${server.info.uri}`)
+});
